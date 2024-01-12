8234d19b8a048e08dbba2a6ff0b2f3f2ba535223
rlawlsdnjs <dlfjs9514@nate.com>
2024-01-12 18:00:17 +0900

    feat : add Logout

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,23 +1,32 @@
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 import tw from 'tailwind-styled-components';
-import { Button } from '../components/common/button';
 import { useRecoilValue, useRecoilState, useResetRecoilState } from 'recoil';
 import { LoginState, User, UserProfile } from '../recoil/sign/atoms/loginState';
-import { useNavigate } from 'react-router-dom';
+
+import { Button } from '../components/common/button';
 import { useEffect } from 'react';
-import { useGetUserData } from '../hooks/auth/getUserData';
+import { useUserData } from '../hooks/auth/putUserData';
 
 export const Header = () => {
   const useLoginState = useRecoilValue(LoginState);
   const userState = useRecoilValue(User);
   const resetLoginState = useResetRecoilState(LoginState);
   const resetUserState = useResetRecoilState(User);
-  const [loginState, setLoginState] = useRecoilState(LoginState);
-  const [userInfo, setUserInfo] = useRecoilState(User);
   const [profile, setProfile] = useRecoilState(UserProfile);
   const navigate = useNavigate();
 
-  const logoutFn = () => {
+  const logoutFn = async () => {
+    const url = 'http://192.168.50.26:8096/api/logout';
+
+    const type = '';
+    let array = { url };
+    const logoutPut = await useUserData(array, type);
+    if (logoutPut) {
+      console.log('로그아웃');
+    }
+
     resetLoginState();
     resetUserState();
     localStorage.removeItem('loginState');
@@ -27,60 +36,39 @@ export const Header = () => {
     navigate('/login');
   };
 
-  const loginToken = localStorage.getItem('loginToken');
-
-  const autoSign = async () => {
-    if (loginToken) {
-      setLoginState(true);
-      setUserInfo({
-        userId: localStorage.getItem('userId'),
-        userPw: localStorage.getItem('userPw'),
-      });
-      const getUserProfile = await useGetUserData(userState.userId);
-      console.log('gd', getUserProfile);
-      setProfile({
-        name: getUserProfile.userName,
-      });
-      localStorage.setItem('loginToken', `${loginToken}`);
-    } else return;
-  };
-
-  useEffect(() => {
-    autoSign();
-  }, []);
-
   return (
     <HeaderSection>
       <div>
-        <h1>
-          {' '}
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
-        </h1>
+        <Link to="/">
+          <h1>
+            <img
+              className="mx-auto h-10 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt="Your Company"
+            />
+          </h1>
+        </Link>
       </div>
       <div>
         <ul className="flex">
           {useLoginState ? (
             <>
-              <li>
+              <HeaderListItem>
                 {profile.name}
                 <span>님</span>
-              </li>
-              <li>
+              </HeaderListItem>
+              <HeaderListItem>
                 <Link
                   to={'my'}
-                  className="rounded-md p-2 flex  w-full  justify-center  bg-white  px-3  py-1.5  text-sm  font-semibold  leading-6 text-indigo-500 shadow-sm  hover:bg-indigo-500 hover:text-white  focus-visible:outline  focus-visible:outline-2  focus-visible:outline-offset-2  focus-visible:outline-indigo-600"
+                  className="rounded-md p-2 flex  w-full  text-sm  font-semibold  justify-center  bg-white  px-3  py-1.5  leading-6 text-indigo-500 shadow-sm  hover:bg-indigo-500 hover:text-white  focus-visible:outline  focus-visible:outline-2  focus-visible:outline-offset-2  focus-visible:outline-indigo-600"
                 >
                   Mypage
                 </Link>
-              </li>
+              </HeaderListItem>
             </>
           ) : null}
 
-          <li>
+          <HeaderListItem>
             {!useLoginState ? (
               <Link
                 to="/login"
@@ -91,7 +79,7 @@ export const Header = () => {
             ) : (
               <Button title="Logout" click={logoutFn}></Button>
             )}
-          </li>
+          </HeaderListItem>
         </ul>
       </div>
     </HeaderSection>
@@ -107,4 +95,12 @@ const HeaderSection = tw.header`
     justify-between
     px-9
     py-3
+`;
+
+const HeaderListItem = tw.li`
+  flex
+  items-center
+  ml-5
+  text-sm  
+  font-semibold 
 `;
