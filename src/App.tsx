fe45c7f75c1494b5736e8b27f01f53a3c6334200
rlawlsdnjs <dlfjs9514@nate.com>
2024-01-12 18:02:32 +0900

    refactor : remove not import

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,26 @@
-import { useState } from 'react';
 import './App.css';
-import axios from 'axios';
-import React from 'react';
-import { Header } from './layout/header';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Login } from './pages/Login';
 import { Sign } from './pages/Sign';
 import { RecoilRoot } from 'recoil';
-import { useRecoilValue } from 'recoil';
-import {
-  User,
-  LoginState,
-  LoginSession,
-  UserSession,
-} from './recoil/sign/atoms/loginState';
 import { MyPage } from './pages/Mypage';
+import { Home } from './Home';
+import { Suspense } from 'react';
+import { Loading } from './components/common/loading';
+import { Admin } from './pages/Admin';
+
 function App() {
   return (
     <RecoilRoot>
-      <Header />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/sign" element={<Sign />} />
-        <Route path="/my" element={<MyPage />} />
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Home />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign" element={<Sign />} />
+          <Route path="/my" element={<MyPage />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </Suspense>
     </RecoilRoot>
   );
 }
