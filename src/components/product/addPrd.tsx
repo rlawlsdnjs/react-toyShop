8a6d12b3f97889d2647a4dbd822086b8e04cc0c5
rlawlsdnjs <dlfjs9514@nate.com>
2024-01-12 17:56:35 +0900

    feat : add Production

---
+++ src/components/product/addPrd.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+import { Button } from '../common/button';
+import Form from '../common/Form';
+import { InputBtn } from '../common/input';
+export const AddPrd = () => {
+  const initialState = {
+    categoryName: '',
+    pdDetail: '',
+    pdName: '',
+    pdPrice: '',
+    pdQuantity: '',
+    pdSize: '',
+    pdStat: '',
+    userEmail: '',
+  };
+
+  const [formData, setFormData] = useState(initialState);
+  const [data, setData] = useState('');
+  const getTitle = (setTitle: string) => {
+    setData(setTitle);
+  };
+  const changeText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevData) => {
+      // 전개 연산자를 사용하여 이전 상태를 복사하고 해당 속성을 업데이트
+      return {
+        ...prevData,
+        [data]: e.target.value,
+      };
+    });
+    console.log(data);
+  };
+
+  useEffect(() => {
+    setData(data);
+    console.log(data);
+  }, [data]);
+  console.log(formData);
+
+  return (
+    <Form>
+      {Object.keys(initialState).map((i, idx) => {
+        return (
+          <InputBtn
+            key={idx}
+            title={i}
+            place={i}
+            getTitle={getTitle}
+            change={changeText}
+          ></InputBtn>
+        );
+      })}
+      <Button
+        //   click={LoginFn}
+        title={'Add product'}
+      ></Button>
+    </Form>
+  );
+};
