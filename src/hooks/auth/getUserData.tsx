dd6b0be534729b0e1241e6dcb33119c64ca47736
rlawlsdnjs <dlfjs9514@nate.com>
2024-01-12 17:58:45 +0900

    refactor : remove not use import

--- src/hooks/auth/getUserData.tsx
+++ src/hooks/auth/getUserData.tsx
@@ -1,12 +1,8 @@
 import axios from 'axios';
-import { useRecoilValue, useRecoilState } from 'recoil';
-import { User, UserProfile } from '../../recoil/sign/atoms/loginState';
-export const useGetUserData = async (user: string) => {
+export const useGetUserData = async (user: string | null) => {
   let url = `http://192.168.50.26:8096/api/profile?userId=${user}`;
   try {
     const response = await axios.get(url);
-
-    console.log(response.data);
 
     return response.data;
   } catch (error) {
